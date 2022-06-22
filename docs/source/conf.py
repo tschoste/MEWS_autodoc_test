58983d69db09ec847d226d8a962e4b2145f8221c
tschoste <106336618+tschoste@users.noreply.github.com>
2022-06-22 15:36:13 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,11 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../mews'))
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/master
 
 # -- Project information -----------------------------------------------------
 
