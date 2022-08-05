6ee7338d6a369823fb7854fa1ea3641095018240
tschoste <106336618+tschoste@users.noreply.github.com>
2022-08-05 08:43:00 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,3 @@
-autodoc_mock_imports = ['mews.cython.markov']
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
