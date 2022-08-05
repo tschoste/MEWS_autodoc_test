eaf2aed7563da452738d8b4f8ea2ce5a7e56abf4
tjschos <tjschos@sandia.gov>
2022-08-05 11:28:27 -0600

    hard reset 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,7 +18,11 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+	'sphinx.ext.napoleon',
+	'sphinx.ext.autodoc',
+	'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +32,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
